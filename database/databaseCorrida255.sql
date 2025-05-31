e66f34f981f10cf5b6551d5709831ab1a63fac7d
ViquePeixoto <victor.peixoto@sptech.school>
2025-05-31 16:57:14 -0300

    Relacionamento N:N criado entre a tabela torneio e a tabela usuário.

--- database/databaseCorrida255.sql
+++ database/databaseCorrida255.sql
@@ -1,4 +1,4 @@
--- Modelagem em script do banco de dados.
+-- Modelagem em script do banco de dados 'corrida255'.
 CREATE DATABASE corrida255;
 USE corrida255;
 
@@ -6,16 +6,28 @@ CREATE TABLE tabelaVeiculo (
 idVeiculo INT PRIMARY KEY AUTO_INCREMENT,
 nomeVeiculo VARCHAR(50),
 tipoVeiculo VARCHAR(9),
-rodaVeiculo VARCHAR(4),
-estiloVeiculo VARCHAR(50)
-    CONSTRAINT checkTipoVeiculo CHECK (in ('Skate', 'Bicicleta', 'Patins', 'Patinete', 'Trike')),
-    CONSTRAINT checkRoda CHECK (in ('Drifter', 'Roadrunner')),
-    CONSTRAINT checkEstilo CHECK (in (
+rodaVeiculo VARCHAR(10),
+estiloVeiculo VARCHAR(50),
+    CONSTRAINT checkTipoVeiculo CHECK (tipoVeiculo IN ('Skate', 'Bicicleta', 'Patins', 'Patinete', 'Trike')),
+    CONSTRAINT checkRoda CHECK (rodaVeiculo IN ('Drifter', 'Roadrunner')),
+    CONSTRAINT checkEstilo CHECK (estiloVeiculo IN (
+        -- Opções para 'skate':
         'Twin Tip', 'Pintail', 'Drop Through', 'Freestyle', 'LDP', 'Downhill', 'Longboard', 'Shortboard', 'Popsicle', 'Old School', 'Mini Cruiser', 'Surfskate',
+
+        -- Opções para 'bicicleta':
         'Mountain', 'BMX', 'Time Trial', 'Track', 'Recumbent', 'Cyclocross', 'Hybrid', 'Road',
+
+        -- Opções para 'patins':
         'Lifestyle', 'Park', 'Vintage', 'Jam', 'Derby',
+
+        -- Opções para 'patinete':
         'Kick', 'Off-Road', 'Three Wheel', 'Folding',
-        'Cargo', 'Recumbent', 'Tadpole', 'Delta', 'Drift', 'Rickshaw'))
+
+        -- Opções para 'trike':
+        'Cargo', 'Recumbent', 'Tadpole', 'Delta', 'Drift', 'Rickshaw',
+
+        -- Estilos secretos encontrados na barra rolante como easter eggs:
+        'Hatsune Miku', 'Cube Land', 'Racionais', 'Bansky', 'Creepypasta' ))
 );
 
 
@@ -29,14 +41,16 @@ descricaoEquipe VARCHAR(50)
 
 
 CREATE TABLE tabelaTorneio (
-idTorneio INT PRIMARY KEY AUTO_INCREMENT,
+idTorneio INT AUTO_INCREMENT,
 nomeTorneio VARCHAR(50),
 tipoTorneio VARCHAR(50),
 descricaoTorneio VARCHAR(50),
 pontoDePartida VARCHAR(50),
 pontoDeChegada VARCHAR(50),
     fkUsuario INT, FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
-    CONSTRAINT checkTipoTorneio CHECK (in ('Ladeira', 'Trick', 'Rei da Colina', 'Mata-mata em equipe', '1v1', 'Cada Um Por Si'))
+PRIMARY KEY (idTorneio, fkUsuario),
+    CONSTRAINT checkTipoTorneio
+        CHECK (in ('Ladeira', 'Trick', 'Rei da Colina', 'Mata-mata em equipe', '1v1', 'Cada Um Por Si'))
 );
 
 
@@ -48,7 +62,20 @@ emailUsuario VARCHAR(50),
 senhaUsuario VARCHAR(50),
 fotoDePerfil VARCHAR(100),
 vitorias INT,
-    fkVeiculo INT, FOREIGN KEY (fkVeiculo) REFERENCES veiculo(idVeiculo),
-    fkEquipe INT, FOREIGN KEY (fkEquipe) REFERENCES equipe(idEquipe),
-    fkLiderDeEquipe INT, FOREIGN KEY (fkLiderDeEquipe) REFERENCES usuario(idUsuario)
+    fkVeiculo INT UNIQUE,
+        FOREIGN KEY (fkVeiculo) REFERENCES tabelaVeiculo(idVeiculo),
+    fkEquipe INT,
+        FOREIGN KEY (fkEquipe) REFERENCES tabelaEquipe(idEquipe),
+    fkLiderDeEquipe INT,
+        FOREIGN KEY (fkLiderDeEquipe) REFERENCES tabelaUsuario(idUsuario)
+);
+
+
+
+CREATE TABLE tabelaTorneioDeUsuario (
+idUsuario INT,
+    FOREIGN KEY (idUsuario) REFERENCES tabelaUsuario(idUsuario),
+idTorneio INT,
+    FOREIGN KEY (idTorneio) REFERENCES tabelaTorneio(idTorneio),
+PRIMARY KEY (idUsuario, idTorneio)
 );
